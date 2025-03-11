4a7391d6579d14755abab2d1c6fea026952bb6a7
Maximon9 <aamaximon989@gmail.com>
2025-03-10 19:51:04 -0500

    Updated: buildspec.json, plugin-main.c;

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -18,45 +18,102 @@ with this program. If not, see <https://www.gnu.org/licenses/>
 
 #include <obs-module.h>
 #include <plugin-support.h>
+#include <obs-module.h>
+#include <util/threading.h>
+#include <util/platform.h>
+#include <util/darray.h>
+#include <util/dstr.h>
+#include <util/deque.h>
+#include <plugin-support.h>
+#include "playlist.h"
 
 OBS_DECLARE_MODULE()
 OBS_MODULE_USE_DEFAULT_LOCALE(PLUGIN_NAME, "en-US")
 
-static uint32_t sequential_media_source_width(void *data)
+static uint32_t playlist_source_width(void *data)
 {
 	return 1920;
 }
 
-static uint32_t sequential_media_source_height(void *data)
+static uint32_t playlist_source_height(void *data)
 {
 	return 1080;
 }
 
-static void *sequential_media_source_create(obs_data_t *settings, obs_source_t *source)
+struct playlist_source {
+	obs_source_t *source;
+	obs_source_t *current_media_source;
+
+	bool shuffle;
+	bool loop;
+	bool paused;
+	bool user_stopped;
+	bool use_hw_decoding;
+	bool close_when_inactive;
+	pthread_mutex_t mutex;
+	DARRAY(struct media_file_data) files;
+	struct media_file_data *current_media; // only for file/folder in the list
+	struct media_file_data *actual_media;  // for both files and folder items
+	size_t current_media_index;
+	char *current_media_filename; // only used with folder_items
+	// to know if current_folder_item_index will be used, check if current file is a folder
+	size_t current_folder_item_index;
+	long long speed;
+	bool first_update;
+
+	obs_hotkey_id play_pause_hotkey;
+	obs_hotkey_id restart_hotkey;
+	obs_hotkey_id stop_hotkey;
+	obs_hotkey_id next_hotkey;
+	obs_hotkey_id prev_hotkey;
+
+	enum obs_media_state state;
+	enum visibility_behavior visibility_behavior;
+	enum restart_behavior restart_behavior;
+
+	struct deque audio_data[MAX_AUDIO_CHANNELS];
+	struct deque audio_frames;
+	struct deque audio_timestamps;
+	size_t num_channels;
+	pthread_mutex_t audio_mutex;
+};
+
+static void *playlist_source_create(obs_data_t *settings, obs_source_t *source)
 {
+	UNUSED_PARAMETER(settings);
 	obs_log(LOG_INFO, "We made it");
-	return NULL;
+
+	struct playlist_source *mps = bzalloc(sizeof(*mps));
+
+	mps->first_update = true;
+	mps->source = source;
+
+	/* Internal media source */
+	obs_data_t *media_source_data = obs_data_create();
+	obs_data_release(media_source_data);
+
+	return mps;
 }
 
-static void sequential_media_source_destroy(void *data)
+static void playlist_source_destroy(void *data)
 {
 	obs_data_release(data);
 }
 
-static const char *sequential_media_source_name(void *data)
+static const char *playlist_source_name(void *data)
 {
 	return "Sequential Media Source"; // This should match the filename (without extension) in data/
 }
 
-struct obs_source_info sequential_media_source_info = {
+struct obs_source_info playlist_source_info = {
 	.id = "sequential_media_source",
 	.type = OBS_SOURCE_TYPE_INPUT,
 	.output_flags = OBS_SOURCE_VIDEO | OBS_SOURCE_AUDIO,
-	.get_name = sequential_media_source_name,
-	.get_width = sequential_media_source_width,
-	.get_height = sequential_media_source_height,
-	.create = sequential_media_source_create,
-	.destroy = sequential_media_source_destroy,
+	.get_name = playlist_source_name,
+	.get_width = playlist_source_width,
+	.get_height = playlist_source_height,
+	.create = *playlist_source_create,
+	.destroy = playlist_source_destroy,
 	.icon_type = OBS_ICON_TYPE_MEDIA,
 };
 
@@ -67,13 +124,13 @@ bool obs_module_load(void)
 	// 	.type = OBS_SOURCE_TYPE_INPUT,
 	// 	.output_flags = OBS_SOURCE_VIDEO | OBS_SOURCE_AUDIO,
 	// 	.get_name = []() { return "Sequential Media Source"; },
-	// 	.create = sequential_media_source_create,
-	// 	.destroy = sequential_media_source_destroy,
+	// 	.create = playlist_source_create,
+	// 	.destroy = playlist_source_destroy,
 	// 	.get_icon = sequential_media_source_icon, // Assign the icon function
 	// };
 
 	obs_log(LOG_INFO, "plugin loaded successfully (version %s)", PLUGIN_VERSION);
-	obs_register_source(&sequential_media_source_info);
+	obs_register_source(&playlist_source_info);
 	return true;
 }
 
