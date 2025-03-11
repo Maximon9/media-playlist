dd04513e5254ecffa17a93912355aca1b11d788b
Maximon9 <aamaximon989@gmail.com>
2025-03-11 14:53:47 -0500

    Added: playlist_source.c, playlist_source.h; Deleted: playlist.c, playlist.h;

--- src/playlist_source.h
+++ src/playlist_source.h
@@ -5,20 +5,13 @@ static const char *media_filter =
 static const char *video_filter = " (*.mp4 *.mpg *.m4v *.ts *.mov *.mxf *.flv *.mkv *.avi *.gif *.webm);;";
 static const char *audio_filter = " (*.mp3 *.m4a *.mka *.ogg *.aac *.wav *.opus *.flac);;";
 
-static uint32_t playlist_source_width(void *data)
-{
-	return 1920;
-}
+static uint32_t playlist_source_width(void *data);
 
-static uint32_t playlist_source_height(void *data)
-{
-	return 1080;
-}
+static uint32_t playlist_source_height(void *data);
 
 struct playlist_source {
 	bool loop;
 };
-struct playlist_source *playlist = {0};
 
 static void *playlist_source_create(obs_data_t *settings, obs_source_t *source);
 
